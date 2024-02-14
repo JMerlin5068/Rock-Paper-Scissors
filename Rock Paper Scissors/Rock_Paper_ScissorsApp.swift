daa717c6f55ce72b0325608d780b893ebbc8862e
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-02-14 08:12:33 -0600

    Initial Commit

---
+++ Rock Paper Scissors/Rock_Paper_ScissorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rock_Paper_ScissorsApp.swift
+//  Rock Paper Scissors
+//
+//  Created by Student on 2/14/24.
+//
+
+import SwiftUI
+
+@main
+struct Rock_Paper_ScissorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
